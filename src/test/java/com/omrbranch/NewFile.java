b04cf5b27b3a282637c92fcfc614da011da098de
GnanaseeliG <gnanaseeligandhi@gmail.com>
2025-12-24 22:47:23 +0530

    Tester2 new line

--- src/test/java/com/omrbranch/NewFile.java
+++ src/test/java/com/omrbranch/NewFile.java
@@ -4,7 +4,8 @@ public class NewFile {
 	
 	public static void main(String[] args) {
 		System.out.println("HI");
-		System.out.println("Im riya");
+		System.out.println("----------------");
+		System.out.println("Myself is  riya");
 		
 		System.out.println("New");
 	}
